45085ac4127618b01849c96763381ebdd7172c17
Нарек Антонян <narekantonyan2033@gmail.com>
2024-06-20 18:33:46 +0300

    Добавьте файлы проекта.

---
+++ cikl1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите сумму вклада:");
+        decimal depositAmount = Convert.ToDecimal(Console.ReadLine());
+
+        Console.WriteLine("Введите количество месяцев:");
+        int months = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 0; i < months; i++)
+        {
+            depositAmount += depositAmount * 0.07m;
+        }
+
+        Console.WriteLine($"Сумма вклада после {months} месяцев: {depositAmount}");
+    }
+}
